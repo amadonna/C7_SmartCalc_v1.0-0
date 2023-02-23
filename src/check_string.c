8e0af70c62b181a0579197cb328db8705408b4da
Elodia Wythers <elodiawy@et-d6.kzn.21-school.ru>
2023-02-24 01:13:01 +0300

    secomd

---
+++ src/check_string.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+struct st
+{
+    double val;
+    int prior;
+       
+};
+
+
+
+int main() {
+    return 0;
+}
+
+int parser(char *str) {
+    int ret = 0;
+
+    return ret;
+}
+
+/*
+int checker(char *str) {
+    int ret = 0;
+    int stop = 0;
+    for(int i = 0; str[i] + '\0' && stop == 0; i++) {
+
+    }
+    return ret;
+}
+*/
