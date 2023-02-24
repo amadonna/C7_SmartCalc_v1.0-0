30a45cc7a7888d726d2dc2acbe40abc72d3246f2
Elodia Wythers <elodiawy@et-d6.kzn.21-school.ru>
2023-02-24 23:07:49 +0300

    a

--- src/check_string.c
+++ src/check_string.c
@@ -1,12 +1,56 @@
 #include<stdio.h>
 
-struct st
+#include <stdlib.h>
+
+typedef enum {
+    x,
+    number,
+    dot,
+    sum,
+    del,
+    mul,
+    divi,
+    mod,
+    pow,
+    sin,
+    cos,
+    tan,
+    asin,
+    acos,
+    atan,
+    sqrt,
+    ln,
+    log,
+    openB,
+    closeB
+} type_sign;
+
+typedef enum {
+    sh,
+    mid,
+    un,
+    high,
+    brac
+} prior_t;
+
+typedef struct Node
 {
     double val;
-    int prior;
-       
-};
+    prior_t prior;
+    type_sign t_val;
+    struct Node *next;
+} stack_21;
 
+void push(stack_21 **st, double num, prior_t pr, type_sign sign) {
+    stack_21 *st_temp = calloc(1, sizeof(stack_21));
+    st_temp->val = num;
+    st_temp->t_val = sign;
+    st_temp->prior = pr;
+    st_temp->next = *st;
+    *st = st_temp;
+}
+
+stack_21 *get()
 
 
 int main() {
@@ -15,7 +59,7 @@ int main() {
 
 int parser(char *str) {
     int ret = 0;
-
+    stack_21 *st = NULL;
     return ret;
 }
 
