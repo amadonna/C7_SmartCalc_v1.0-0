b64b485aea51e6f7712ea63ee584887b1061c144
Elodia Wythers <elodiawy@et-d6.kzn.21-school.ru>
2023-02-27 23:39:54 +0300

    jj

--- src/check_string.c
+++ src/check_string.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 typedef enum {
-    x,
-    number,
-    dot,
+    unar,
     sum,
     del,
     mul,
     divi,
     mod,
     pow,
-    sin,
-    cos,
-    tan,
-    asin,
-    acos,
-    atan,
-    sqrt,
-    ln,
-    log,
+    coss,
+    sinn,
+    tann,
+    acoss,
+    asinn,
+    atann,
+    sqrtt,
+    lnn,
+    logg,
     openB,
     closeB
 } type_sign;
 
 typedef enum {
-    sh,
-    mid,
-    un,
-    high,
-    brac
+    unary,
+    open_b,
+    close_b,
+    plus_minus,
+    div_mul_mod,
+    expon_funs
 } prior_t;
 
 typedef struct Node
@@ -40,35 +40,47 @@ typedef struct Node
     struct Node *next;
 } stack_symbol;
 
-typedef struct Num
+/*typedef struct Num
 {
     double val;
     struct Num *next;
-} stack_num;
+} stack_num;*/
 
 
 int parser(char *str, stack_symbol **st);
-void push_sym(stack_symbol **st, prior_t pr, type_sign sign);
+void push_sym(stack_symbol **st, type_sign sign);
 stack_symbol *get_sym(stack_symbol **st);
-stack_num *get_num(stack_num **st);
+//stack_num *get_num(stack_num **st);
 int digits(char c);
-double to_double(char *str, int *i);
+char* cut_char(char *str, int *i);
+int is_func(int *i, char *str);
+stack_symbol *watch_peak(stack_symbol *st);
 
-
-void push_sym(stack_symbol **st, prior_t pr, type_sign sign) {
+void push_sym(stack_symbol **st, type_sign sign) {
     stack_symbol *st_temp = calloc(1, sizeof(stack_symbol));
+    if(sign == unar)
+        st_temp->prior = unary; 
+    else if(sign == openB)
+        st_temp->prior = open_b;
+    else if(sign == closeB)
+        st_temp->prior = close_b;
+    else if(sign == sum || sign == del)
+        st_temp->prior = plus_minus;
+    else if(sign == divi || sign == mul || sign == mod)
+        st_temp->prior = div_mul_mod;
+    else if(sign >= 6 && sign <= 15)
+        st_temp->prior = expon_funs;
     st_temp->t_val = sign;
-    st_temp->prior = pr;
     st_temp->next = *st;
     *st = st_temp;
 }
 
-void push_num(stack_num **st, double num) {
+/*void push_num(stack_num **st, double num) {
     stack_num *st_temp = calloc(1, sizeof(stack_symbol));
     st_temp->val = num;
     st_temp->next = *st;
     *st = st_temp;
-}
+}*/
 
 stack_symbol *get_sym(stack_symbol **st) {
     stack_symbol *temp = NULL;
@@ -81,54 +93,73 @@ stack_symbol *get_sym(stack_symbol **st) {
     return ret;
 }
 
-stack_num *get_num(stack_num **st) {
-    stack_num *temp = NULL;
-    stack_num *ret = calloc(1, sizeof(stack_symbol));
-    temp = *st;
-    ret->val = temp->val;
-    *st = (*st)->next;
-    free(temp);
-    return ret;
-}
 
 
-int main() {
-    stack_num *st = NULL;
+stack_symbol *watch_peak(stack_symbol *st) {
     stack_symbol *temp = NULL;
-    st = calloc(4, sizeof(stack_symbol));
-    temp = calloc(4, sizeof(stack_symbol));
-    for (int i = 0; i < 4; i++) {
-        push_sym(&temp,  i, i);
-        push_num(&st, (double)i);
-    }
-
-    for (int j = 0; j < 4; j++) {
-        printf("pr = %d\ttype = %d", temp->prior, temp->t_val);
-        printf("\t\tpr = %lf\n", st->val);
-        get_sym(&temp);
-        get_num(&st);
-    }
-    free(st);
-    free(temp);
-    return 0;
+    temp = get_sym(&st);
+    push_sym(&temp, temp->t_val);
+    return temp;
 }
 
-
-int to_stack(char *str, stack_symbol **st, stack_num **num) {
+int to_stack(char *str, stack_symbol **st, char **outs) {
     int ret = 0;
+    int c_out = 0;
+    int check_func = 0;
     for(int i = 0; str[i] != '\0'; i++) {
         if(isspace(str[i]))
             i++;
         if(digits(str[i]) == 1) {
-            push_num(num, to_double(str, &i));
-        } else if()
+            strcpy(*outs + strlen(*outs), cut_char(str, &i));
+            *outs[strlen(*outs)] = '\t';
+        } else if((check_func = is_func(&i, str)) != 0)
+            push_sym(st, check_func);
+        else if()
         
     }
     return ret;
 }
 
-double to_double(char *str, int *i) {
-    double res = 0;
+
+int is_oper(char *str, int *i) {
+    char c = 0;
+    c = str[*i];
+    if()
+}
+
+int is_func(int *i, char *str) {
+    int ret = 0;
+    if(str[*i] >= 'a' && str[*i] <= 'z') {
+        if(strncmp("cos", str + *i, 3) == 0)
+            ret = coss;
+        else if(strncmp("sin", str + *i, 3) == 0)
+            ret = sinn;
+        else if(strncmp("tan", str + *i, 3) == 0)
+            ret = tann;
+        else if(strncmp("acos", str + *i, 4) == 0)
+            ret = acoss;
+        else if(strncmp("asin", str + *i, 4) == 0)
+            ret = asinn;
+        else if(strncmp("atan", str + *i, 4) == 0)
+            ret = atann;
+        else if(strncmp("sqrt", str + *i, 4) == 0)
+            ret = sqrtt;
+        else if(strncmp("ln", str + *i, 4) == 0)
+            ret = lnn;
+        else if(strncmp("log", str + *i, 4) == 0)
+            ret = logg;
+    }
+    if(ret == tann || ret == sinn || ret == coss || ret == logg)
+        *i += 3;
+    else if(ret == acoss || ret == asinn || ret == atann || ret == sqrtt)
+        *i += 4;
+    else if(ret == lnn)
+        *i += 2;
+    return ret;
+}
+
+char* cut_char(char *str, int *i) {
+    char *ret = NULL;
     int j = 0;
     char temp[32] = {'\0'};
     while((digits(str[*i]) == 1) || (str[*i] == '.' && digits(str[*i - 1]) == 1 && digits(str[*i + 1]) == 1)) {
@@ -136,8 +167,8 @@ double to_double(char *str, int *i) {
         j++;
         *i += 1;
     }
-    res = atof(temp);
-    return res;
+    ret = temp;
+    return ret;
 }
 
 int digits(char c) {
@@ -160,3 +191,12 @@ int checker(char *str) {
     return ret;
 }
 */
+/*stack_num *get_num(stack_num **st) {
+    stack_num *temp = NULL;
+    stack_num *ret = calloc(1, sizeof(stack_symbol));
+    temp = *st;
+    ret->val = temp->val;
+    *st = (*st)->next;
+    free(temp);
+    return ret;
+}*/
